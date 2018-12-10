ee1ab2ab74d166940e3d35466c855d17c86e1bcc
Andrew Kachanov <andrew.kachanov@gmail.com>
2018-12-10 15:10:29 +0300

    Added gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    'gatsby-plugin-netlify', // make sure to put last in the array
   ],
 }
